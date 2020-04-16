050402a13adadfa7c4bc6af7370417d2e3b88f89
rurugy15 <47134003+rurugy15@users.noreply.github.com>
2020-04-16 15:20:42 +0900

    일단 타이틀 화면만 구현

--- Assets/Scripts/SceneSctipts/TitleScene.cs
+++ Assets/Scripts/SceneSctipts/TitleScene.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour
 {
@@ -10,16 +11,50 @@ public class TitleScene : MonoBehaviour
     public Text[] firstText;
     private bool first;
 
+    public Image titleImage;
+    private bool second;
+
+    public Text secondText;
+    private bool canPress;
+
     // Start is called before the first frame update
     void Start()
     {
+        titleImage.gameObject.SetActive(false);
+        secondText.color = new Color(secondText.color.r, secondText.color.b, secondText.color.g, 0);
+        secondText.gameObject.SetActive(false);
         changeAlpha(0f);
         for (int i = 0; i < firstText.Length; i++)
         {
             firstText[i].gameObject.SetActive(false);
         }
+        canPress = false;
         first = false;
+        second = false;
         StartCoroutine(Title());
+    }
+
+    void Update()
+    {
+        if(first)
+        {
+            StartCoroutine(TitleFadeIn());
+            first = false;
+        }
+        if(second)
+        {
+            canPress = true;
+            StartCoroutine(PressTitle());
+            second = false;
+        }
+        if(canPress)
+        {
+            bool c_pressed = Input.GetKeyDown(KeyCode.C);
+            if (c_pressed)
+            {
+                SceneManager.LoadScene("1");
+            }
+        }
     }
 
     IEnumerator Title()
@@ -46,6 +81,61 @@ public class TitleScene : MonoBehaviour
             yield return null;
         }
         changeAlpha(0f);
+        yield return new WaitForSeconds(0.5f);
+        first = true;
+    }
+
+    IEnumerator TitleFadeIn()
+    {
+        elapsed = 0f;
+        float fadeTime = 0.4f;
+        titleImage.gameObject.SetActive(true);
+        while(true)
+        {
+            elapsed += Time.deltaTime;
+            if(elapsed<fadeTime)
+            {
+                titleImage.color = new Color(titleImage.color.r, titleImage.color.b, titleImage.color.g, elapsed/fadeTime);
+            }
+            else
+            {
+                titleImage.color = new Color(titleImage.color.r, titleImage.color.b, titleImage.color.g, 1f);
+                break;
+            }
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.4f);
+        second = true;
+    }
+
+    IEnumerator PressTitle()
+    {
+        elapsed = 0;
+        secondText.gameObject.SetActive(true);
+
+        while(true)
+        {
+            float fadeTime = 0.6f;
+            elapsed += Time.deltaTime;
+            if(elapsed< fadeTime)
+            {
+                secondText.color = new Color(secondText.color.r, secondText.color.b, secondText.color.g, elapsed/ fadeTime);
+            }
+            else if(fadeTime <= elapsed && elapsed< fadeTime + 0.6f)
+            {
+                secondText.color = new Color(secondText.color.r, secondText.color.b, secondText.color.g, 1f);
+            }
+            else if(elapsed>= fadeTime + 0.6f && elapsed< 2*fadeTime + 0.6f)
+            {
+                secondText.color = new Color(secondText.color.r, secondText.color.b, secondText.color.g, (2 * fadeTime + 0.6f - elapsed)/ fadeTime);
+            }
+            else
+            {
+                secondText.color = new Color(secondText.color.r, secondText.color.b, secondText.color.g, 0f);
+                elapsed = 0f;
+            }
+            yield return null;
+        }
     }
 
     void changeAlpha(float value)
