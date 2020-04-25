e3d8ac1554fb3d9bc8a14b89062743765a71e6d4
Jaehyun Ahn <blackhan25@daum.net>
2020-04-25 12:13:50 +0900

    씬 연결

---
+++ Assets/Scripts/AlphaTestStartScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AlphaTestStartScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        SceneManager.LoadScene("AlphaTestStartScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string SceneNum)
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+}
