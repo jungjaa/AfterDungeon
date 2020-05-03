365b153baa69a6c7ee422768a23b41eee313b54b
rurugy15 <47134003+rurugy15@users.noreply.github.com>
2020-05-03 15:08:40 +0900

    떨어지는 바닥 거의 구현

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -152,6 +152,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Debug.Log("Time: " + Time.deltaTime);
+        Debug.Log(rb2D.velocity.y);
         VelocityLimit();
         isGrounded = GroundChecking();
         closestWall = WallChecking();
--- Assets/Scripts/Stage3/FallingPlatform.cs
+++ Assets/Scripts/Stage3/FallingPlatform.cs
@@ -12,11 +12,32 @@ public class FallingPlatform : MonoBehaviour
 
     public Transform leftPoint;
     public Transform rightPoint;
+    public Transform weightChecker;
 
-    private Vector2 checkBox = new Vector2(0.05f,0.05f);
+    private Vector2 checkBox = new Vector2(0.5f,0.5f);
+    private Vector2 weightBox = new Vector2(0.95f, 0.2f);
+
+    [Tooltip("추락 상호작용할 Layer")]
+    [SerializeField] private LayerMask whatIsWeight;
+
+    private Rigidbody2D rb2D;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(weightChecker.position, weightBox);
+        /*
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(leftPoint.position, checkBox);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(rightPoint.position, checkBox);
+        */
+    }
     // Start is called before the first frame update
     void Start()
     {
+        rb2D = GetComponent<Rigidbody2D>();
         left = CheckSidePlatform(leftPoint.position);
         right = CheckSidePlatform(rightPoint.position);
     }
@@ -26,12 +47,45 @@ public class FallingPlatform : MonoBehaviour
     {
         if(isFalling)
         {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y - 1.1772f); //만약 player의 gravityscale이 달라진다면 이것도 수정해야 함... 6 기준임
+            VelocityLimit();
             elapsed += Time.deltaTime;
         }
-        if(elapsed>2f)
+        else if(WeightChecking())
         {
-            Destroy(gameObject);
+            RecursiveFalling();
         }
+
+    }
+
+    private void VelocityLimit()
+    {
+        if (rb2D.velocity.y < (-1) * 20/*jumpVelocity.y*/ + 0.2f /* * rb2D.gravityScale / 6*/)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, (-1) * 20/*jumpVelocity.y*/ + 0.2f /* * rb2D.gravityScale / 6*/);
+        }
+    }
+    private bool WeightChecking()
+    {
+        List<Collider2D> colliders = new List<Collider2D>();
+
+        Collider2D[] colls = Physics2D.OverlapBoxAll(weightChecker.position, weightBox, 0, whatIsWeight);
+        foreach (Collider2D coll in colls)
+        {
+            colliders.Add(coll);
+        }
+
+        for (int i = 0; i < colliders.Count; i++)
+        {
+            if (colliders[i].gameObject != gameObject && colliders[i].gameObject.GetComponent<Rigidbody2D>()!=null)
+            {
+                if (colliders[i].gameObject.GetComponent<Rigidbody2D>().velocity.y == 0 && colliders[i].gameObject.transform.position.y>=transform.position.y)
+                    return true;
+            }
+
+        }
+        return false;
+
     }
 
     protected void RecursiveFalling()
@@ -39,18 +93,17 @@ public class FallingPlatform : MonoBehaviour
         if (isFalling)
             return;
 
+        isFalling = true;
         if (left != null)
             left.RecursiveFalling();
         if (right != null)
             right.RecursiveFalling();
-
-        isFalling = true;
     }
 
     FallingPlatform CheckSidePlatform(Vector2 checkPosition)
     {
         List<Collider2D> colliders = new List<Collider2D>();
-        Collider2D[] colls = Physics2D.OverlapBoxAll(checkPosition, checkBox, 0);
+        Collider2D[] colls = Physics2D.OverlapBoxAll(checkPosition, checkBox,0);
         foreach (Collider2D coll in colls)
         {
             colliders.Add(coll);
@@ -65,4 +118,6 @@ public class FallingPlatform : MonoBehaviour
         }
         return null;
     }
+
+
 }
