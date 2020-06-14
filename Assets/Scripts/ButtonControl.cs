b0a57fde246529f0100b2ab6a75572b986258b28
rurugy15 <47134003+rurugy15@users.noreply.github.com>
2020-06-14 16:34:30 +0900

    버튼 프리팹 추가

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    [SerializeField] private Sprite unPressed;
+    [SerializeField] private Sprite Pressed;
+    private Sprite next;
+    SpriteRenderer spr;
+    private float elapsed = 0f;
+    private bool isChanged = false;
+
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        spr.sprite = unPressed;
+        next = Pressed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if(!isChanged && elapsed<0.2f)
+        {
+            isChanged = true;
+            Sprite temp = spr.sprite;
+            spr.sprite = next;
+            next = temp;
+        }
+        else if(elapsed>0.22f && isChanged)
+        {
+            Sprite temp = spr.sprite;
+            spr.sprite = next;
+            next = temp;
+            isChanged = false;
+        }
+        else if(elapsed>1.5f)
+        {
+            elapsed = 0f;
+        }
+        
+    }
+
+}
